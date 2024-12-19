cb6f19b9f008d3bf4ac5b7ddff739c07c9f598f0
Levi <70198955+Draysongz@users.noreply.github.com>
2024-12-19 15:34:57 +0100

    done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,44 +1,44 @@
-"use client"
+// "use client"
 
-import { RiArrowLeftSFill } from "react-icons/ri";
-import { FaEllipsisVertical, FaChevronDown } from "react-icons/fa6";
-import { useState } from "react";
-import Link from "next/link";
+// import { RiArrowLeftSFill } from "react-icons/ri";
+// import { FaEllipsisVertical, FaChevronDown } from "react-icons/fa6";
+// import { useState } from "react";
+// import Link from "next/link";
 
-const Header = () => {
+// const Header = () => {
 
-    const [ openMenu, setOpenMenu ] = useState(false);
+//     const [ openMenu, setOpenMenu ] = useState(false);
 
-    const navItems = [
-        {
-            name: "Open Bot"
-        },
-        {
-            name: "Reload Page"
-        },
-        {
-            name: "Terms of Use"
-        },
-    ]
+//     const navItems = [
+//         {
+//             name: "Open Bot"
+//         },
+//         {
+//             name: "Reload Page"
+//         },
+//         {
+//             name: "Terms of Use"
+//         },
+//     ]
 
-  return (
-    <header className='w-full flex justify-center items-center h-[50px] bg-[#121113] absolute top-0 z-10'>
-        <menu className='w-[90%] flex justify-between items-center relative'>
-            <RiArrowLeftSFill className="text-white text-2xl" />
-            <span className="text-2xl text-white w-fit">亚太百事通</span>
-            <span className="flex w-[55px] justify-between items-center text-white text-xl">
-                <FaChevronDown />
-                <FaEllipsisVertical onClick={() => setOpenMenu(!openMenu)} />
-            </span>
+//   return (
+//     <header className='w-full flex justify-center items-center h-[50px] bg-[#121113] absolute top-0 z-10'>
+//         <menu className='w-[90%] flex justify-between items-center relative'>
+//             <RiArrowLeftSFill className="text-white text-2xl" />
+//             <span className="text-2xl text-white w-fit">亚太百事通</span>
+//             <span className="flex w-[55px] justify-between items-center text-white text-xl">
+//                 <FaChevronDown />
+//                 <FaEllipsisVertical onClick={() => setOpenMenu(!openMenu)} />
+//             </span>
 
-            <nav className={`flex w-[180px] border border-[#FDD902] flex-col items-center justify-evenly h-[150px] bg-[#121113] absolute top-10 right-0 ${openMenu ? "visible" : "hidden"}`}>
-                {navItems.map((item, index) => (
-                    <Link key={index} href="#">{item.name}</Link>
-                ))}
-            </nav>
-        </menu>
-    </header>
-  )
-}
+//             <nav className={`flex w-[180px] border border-[#FDD902] flex-col items-center justify-evenly h-[150px] bg-[#121113] absolute top-10 right-0 ${openMenu ? "visible" : "hidden"}`}>
+//                 {navItems.map((item, index) => (
+//                     <Link key={index} href="#">{item.name}</Link>
+//                 ))}
+//             </nav>
+//         </menu>
+//     </header>
+//   )
+// }
 
-export default Header
+// export default Header
