9b8fe3b246d23470c4178ca0688724d0a32bb879
Sunday Solomon <nomolos2019@gmail.com>
2024-12-22 08:21:09 +0100

    restaurant page

---
+++ app/restaurant/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { Resturant } from '@/components'
+// import { useState } from 'react'
+
+
+const Restaurant = () => {
+
+    // const [ restorant, setRestorant ] = useState(0);
+
+  return (
+    <div><Resturant index={0} /></div>
+  )
+}
+
+export default Restaurant
